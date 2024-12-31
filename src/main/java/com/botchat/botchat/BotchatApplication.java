ae429ebe2e6d7c91928cbf4fd0edd24f456c243a
Daniel <danielbarcheta@gmail.com>
2024-12-31 12:42:05 -0300

    first-commit

---
+++ src/main/java/com/botchat/botchat/BotchatApplication.java
@@ -0,0 +1,13 @@
+package com.botchat.botchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/botchat/botchat/BotchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botchat.botchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
